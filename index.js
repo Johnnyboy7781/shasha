f0659b10392fb73a5c7f80760eeb67fde1d4a98c
vpham26 <vulong.pham26@gmail.com>
2022-03-31 20:31:29 -0400

    modified public and views folders' items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const exhbs = requre('express-handlebars');
+const exphbs = require('express-handlebars');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -12,8 +12,8 @@ app.set("view engine","hbs");
 app.engine('hbs',exphbs({
     extname: 'hbs',
     defaultLayout: 'index'
-    layoutsDir: __dirname + 'views/layouts',
-    partialsDir: __dirname + 'views/partials',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
 }));
 
 const port = 8900;
@@ -22,5 +22,5 @@ console.log(`Listening to server: http://localhost:${port}`);
 
 // Landing page
 app.get('/', (reg,res)=>{
-    res.send("Hello!!");
+    res.render("main",{message: 'Hi there!'});
 })
